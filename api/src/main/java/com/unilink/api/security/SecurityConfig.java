219f136342e64f07d1ab11bcc1a91e0705455224
João Loss <joaoloss@Joaos-MacBook-Air.local>
2025-08-13 14:08:32 -0300

    fix: removing filters

--- api/src/main/java/com/unilink/api/security/SecurityConfig.java
+++ api/src/main/java/com/unilink/api/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+// import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -17,7 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.unilink.api.enums.UserRole;
+// import com.unilink.api.enums.UserRole;
 
 @Configuration
 @EnableWebSecurity
@@ -33,18 +33,18 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/tags").authenticated()
-                .requestMatchers(HttpMethod.PUT, "/api/projects/**").authenticated() // more verifications done in service layer
-                .requestMatchers(HttpMethod.GET, "/api/centers", "/api/centers/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/tags", "/api/tags/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/projects", "/api/projects/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/test").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/users").hasRole(UserRole.SUPER_ADMIN.getValue())                
-                .requestMatchers(HttpMethod.POST).hasRole(UserRole.SUPER_ADMIN.getValue())
-                .requestMatchers(HttpMethod.PUT).hasRole(UserRole.SUPER_ADMIN.getValue())
+                // .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                // .requestMatchers(HttpMethod.POST, "/api/tags").authenticated()
+                // .requestMatchers(HttpMethod.PUT, "/api/projects/**").authenticated() // more verifications done in service layer
+                // .requestMatchers(HttpMethod.GET, "/api/centers", "/api/centers/**").permitAll()
+                // .requestMatchers(HttpMethod.GET, "/api/tags", "/api/tags/**").permitAll()
+                // .requestMatchers(HttpMethod.GET, "/api/projects", "/api/projects/**").permitAll()
+                // .requestMatchers(HttpMethod.GET, "/api/test").permitAll()
+                // .requestMatchers(HttpMethod.GET, "/api/users").hasRole(UserRole.SUPER_ADMIN.getValue())                
+                // .requestMatchers(HttpMethod.POST).hasRole(UserRole.SUPER_ADMIN.getValue())
+                // .requestMatchers(HttpMethod.PUT).hasRole(UserRole.SUPER_ADMIN.getValue())
 
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs/**", "/api-docs", "/swagger-ui.html/**").permitAll()
+                // .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs/**", "/api-docs", "/swagger-ui.html/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
